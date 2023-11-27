37806c3ae69f9af8621b3a1d96a1f98f12d66631
Edu <vargaseduardo517@gmail.com>
2023-11-27 16:40:59 -0300

    fix: corregimos una linea de comentario q no debia estar ahi

--- Backend/src/setting/config.js
+++ Backend/src/setting/config.js
@@ -2,7 +2,7 @@ import {config as dotenv} from "dotenv"                     // le cambie el nomb
 dotenv()
 
 export const config = {
-    port: process.env.PORT || 4000,                          // EL PORT en mayusculas pq en mi archivo ".env" tenemos "PORT: 3001".
+    port: process.env.PORT || 4000,                          
     mongo_uri: process.env.MONGO_URI,
     jwtSecret: process.env.JWT_SECRET, 
 }
