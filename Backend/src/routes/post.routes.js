302688ff128b9608b498ab9d9f4bfeb286b41d9e
Edu <vargaseduardo517@gmail.com>
2023-11-30 19:10:09 -0300

    fix: corregimos un pequeño error donde pediamos una autenticacion para obtener todos los posts, y tambien verificamos q no necesitamos del controlador para cerrar sesion

--- Backend/src/routes/post.routes.js
+++ Backend/src/routes/post.routes.js
@@ -5,10 +5,15 @@ import {
   ctrlGetPosts,
   ctrlEditPost,
 } from "../controllers/post.controller.js";
+import { authMiddleware } from "../middleware/authentication.js";
 
 const postRouter = Router();
 
 postRouter.get("/", ctrlGetPosts); // obtener los posts
+
+// esto para q a partir de ahora, crear, edit y elim un post van a tener q estar autenticados
+postRouter.use(authMiddleware)
+
 postRouter.post("/createPost", ctrlCreatePost); // Crear post
 postRouter.put("/:postId", ctrlEditPost); // Editar una publicación
 postRouter.delete("/:postId", ctrlDeletePost); // Eliminar Post
