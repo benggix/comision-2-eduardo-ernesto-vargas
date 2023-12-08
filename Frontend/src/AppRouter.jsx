dd4daa1b231d9749c3f36a8e4692d95a0038e9f8
Edu <vargaseduardo517@gmail.com>
2023-12-07 22:12:16 -0300

    feat: Creamos los compoentes para crear, editar post y sus vistas correspondiendo junto a sus rutas definidas

--- Frontend/src/AppRouter.jsx
+++ Frontend/src/AppRouter.jsx
@@ -3,16 +3,20 @@ import { HomePage } from "./views/HomePage";
 import { RegisterPage } from "./views/RegisterPage";
 import { LoginPage } from "./views/LoginPage";
 import { PostPage } from "./views/PostPage";
+import { CreatePost } from "./components/CreatePost";
+import { EditPostPage } from "./views/EditPostPage";
+import { NotFoundPage } from "./views/NotFoundPage";
 
 export const AppRouter = () => {
   return (
     <Routes>
+      <Route path="*" element={<NotFoundPage />} />
       <Route path="/" element={<HomePage />} />
       <Route path="/users/login" element={<LoginPage />} />
-      <Route path="/users/register" element={<RegisterPage/>} />
-
-
-      <Route path="/posts" element={<PostPage/>} />
+      <Route path="/users/register" element={<RegisterPage />} />
+      <Route path="/posts" element={<PostPage />} />
+      <Route path="/posts/createPost" element={<CreatePost />} />
+      <Route path="/posts/:postId" element={<EditPostPage />} />
     </Routes>
   );
 };
---
+++ Frontend/src/views/EditPostPage.jsx
@@ -0,0 +1,7 @@
+import { EditPost } from "../components/EditPost"
+
+export const EditPostPage = () => {
+  return (
+    <EditPost/>
+  )
+}
